d000fd6e3b7a08e4e4c5a599ad805a2b0e8efc5e
UnknownLvl4 <hongocduyyu@gmail.com>
2025-04-30 00:28:53 +0700

    git ignore global

--- Backend/dist/entities/product.entity.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Product {
-    category_id: string;
-    product_id: string;
-    product_name: string;
-    description: string;
-    stock_quantity: number;
-    image: string;
-    price: number;
-}
